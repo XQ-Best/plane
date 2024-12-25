729db8bfd5a01ece5897e1134fb5f4a6faa090e2
xuquan <xuquan2003@outlook.com>
2024-12-25 23:37:05 +0800

    Initial Commit

---
+++ plane/planeApp.swift
@@ -0,0 +1,17 @@
+//
+//  planeApp.swift
+//  plane
+//
+//  Created by 徐铨 on 2024/12/25.
+//
+
+import SwiftUI
+
+@main
+struct planeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ planeTests/planeTests.swift
@@ -0,0 +1,17 @@
+//
+//  planeTests.swift
+//  planeTests
+//
+//  Created by 徐铨 on 2024/12/25.
+//
+
+import Testing
+@testable import plane
+
+struct planeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
